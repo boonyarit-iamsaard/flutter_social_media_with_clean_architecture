043e7fbecef1142b07de9d894667f0c7e24b068b
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2023-09-22 16:44:46 +0700

    feat(auth): add authenticated user entity validation

--- lib/src/features/auth/domain/entities/authenticated_user.dart
+++ lib/src/features/auth/domain/entities/authenticated_user.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_social_media_with_clean_architecture/src/shared/domain/entities/user.dart';
+import 'package:formz/formz.dart';
 
 class AuthenticatedUserEntity extends UserEntity {
   const AuthenticatedUserEntity({
@@ -10,12 +11,12 @@ class AuthenticatedUserEntity extends UserEntity {
     this.email,
   });
 
-  final String? email;
+  final Email? email;
 
   static empty() => const AuthenticatedUserEntity(
         id: 'user_0',
-        username: '-',
-        email: '-',
+        username: Username.pure(),
+        email: Email.pure(),
       );
 
   @override
@@ -30,11 +31,11 @@ class AuthenticatedUserEntity extends UserEntity {
 
   AuthenticatedUserEntity copyWith({
     String? id,
-    String? username,
+    Username? username,
     int? followers,
     int? following,
     String? imagePath,
-    String? email,
+    Email? email,
   }) {
     return AuthenticatedUserEntity(
       id: id ?? this.id,
@@ -44,5 +45,41 @@ class AuthenticatedUserEntity extends UserEntity {
       imagePath: imagePath ?? this.imagePath,
       email: email ?? this.email,
     );
+  }
+}
+
+enum EmailValidationError { invalid }
+
+class Email extends FormzInput<String, EmailValidationError> {
+  const Email.pure() : super.pure('');
+
+  const Email.dirty([String value = '']) : super.dirty(value);
+
+  static final _emailRegExp = RegExp(
+    r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+  );
+
+  @override
+  EmailValidationError? validator(value) {
+    return _emailRegExp.hasMatch(value) ? null : EmailValidationError.invalid;
+  }
+}
+
+enum PasswordValidationError { invalid }
+
+class Password extends FormzInput<String, PasswordValidationError> {
+  const Password.pure() : super.pure('');
+
+  const Password.dirty([String value = '']) : super.dirty(value);
+
+  static final _passwordRegExp = RegExp(
+    r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$',
+  );
+
+  @override
+  PasswordValidationError? validator(value) {
+    return _passwordRegExp.hasMatch(value)
+        ? null
+        : PasswordValidationError.invalid;
   }
 }
